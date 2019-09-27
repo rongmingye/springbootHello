136cbe15d8bd5cb0106c3153f8fac3e4da3926e4
rongmingye <1164898915@qq.com>
2019-09-27 17:15:00 +0800

    修改启动类

--- src/main/java/com/example/helloworld/ProjectHelloworldApplication.java
+++ src/main/java/com/example/helloworld/ProjectHelloworldApplication.java
@@ -1,8 +1,12 @@
 package com.example.helloworld;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
+@MapperScan("com.example.helloworld.mapper")
 @SpringBootApplication
 public class ProjectHelloworldApplication {
 
