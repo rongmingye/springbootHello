1c0733d66104eba7e9620082e455f02f52230aac
rongmingye <1164898915@qq.com>
2019-09-27 17:14:03 +0800

    info控制类

---
+++ src/main/java/com/example/helloworld/controller/InfoController.java
@@ -0,0 +1,33 @@
+package com.example.helloworld.controller;
+
+
+import com.example.helloworld.common.api.ApiBaseResultCode;
+import com.example.helloworld.common.api.ApiResponse;
+import com.example.helloworld.entity.Info;
+import com.example.helloworld.service.InfoService;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class InfoController {
+
+    private InfoService infoService;
+
+    @RequestMapping("add")
+    public ApiResponse<Info> add(Info info) {
+        if (info == null || StringUtils.isEmpty(info.getName())) {
+            return ApiResponse.failure(ApiBaseResultCode.PARAMSFAILED.code(), "缺少参数：name");
+        }
+
+        Info newInfo = infoService.addInfo(info, true);
+        if (newInfo == null) {
+            return ApiResponse.failure(ApiBaseResultCode.FAILURE.code(), "系统出错，创建失败，请联系系统管理员");
+        } else {
+            ApiResponse response = ApiResponse.success("内容创建成功");
+            response.setData("");
+            return response;
+        }
+    }
+}
