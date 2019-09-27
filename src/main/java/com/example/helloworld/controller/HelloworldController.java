a601f53e2331895dc8c0c76593da3f3cdf5192c0
rongmingye <1164898915@qq.com>
2019-09-27 14:50:49 +0800

    hello接口处理

---
+++ src/main/java/com/example/helloworld/controller/HelloworldController.java
@@ -0,0 +1,14 @@
+package com.example.helloworld.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HelloworldController {
+
+    @RequestMapping("hello")
+    public String hello() {
+        return "hello world";
+    }
+}
