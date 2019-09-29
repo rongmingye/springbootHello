96abaa3e26d4267c02370f39bbab4cb1c8f32653
rongmingye <rmy@ideamake.cn>
2019-09-29 15:50:12 +0800

    修改Info实现类, 解决InfoService的bean找不到的问题

--- src/main/java/com/example/helloworld/service/impl/InfoServiceImpl.java
+++ src/main/java/com/example/helloworld/service/impl/InfoServiceImpl.java
@@ -1,13 +1,15 @@
 package com.example.helloworld.service.impl;
 
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.example.helloworld.entity.Info;
 import com.example.helloworld.mapper.InfoMapper;
 import com.example.helloworld.service.InfoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
 
 // 实现类
 
+@Service
 public class InfoServiceImpl extends ServiceImpl<InfoMapper, Info> implements InfoService {
 
     @Autowired
