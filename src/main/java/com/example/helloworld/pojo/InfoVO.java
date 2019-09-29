e163f36b0b60443f7e38dd4e73e2ccd8f1efb397
rongmingye <1164898915@qq.com>
2019-09-29 23:59:15 +0800

    新增InfoVO

---
+++ src/main/java/com/example/helloworld/pojo/InfoVO.java
@@ -0,0 +1,15 @@
+package com.example.helloworld.pojo;
+
+import lombok.Data;
+
+@Data
+public class InfoVO {
+
+    private String id;
+
+    private String name;
+
+    private String content;
+
+    private String type;
+}
